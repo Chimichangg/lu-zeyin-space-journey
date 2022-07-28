f6dcef55e6516ecf360f9c68a251ceee9f1546b7
Hephaestus <zeyinlu@Hephaestus.local>
2022-07-27 21:16:25 -0400

    finshing audio drag and drop fucntion

--- js/main.js
+++ js/main.js
@@ -3,9 +3,32 @@
         jet = document.querySelector('.jet-area svg'),
         jetItem = document.querySelectorAll('.jet-area g'),
         playBut = document.querySelector('#play-but'),
-        pauseBut = document.querySelector('#pause-but');
+        pauseBut = document.querySelector('#pause-but'),
+        allAudio = document.querySelectorAll('audio')
+        ;
+    
+    
     
 
+    allAudio.forEach(clip => {clip.volume = 0})
+
+    
+    function startStop () {
+        document.querySelector("#body").classList.toggle("move-bg");
+        document.querySelector(".fire").classList.toggle("hidden");
+    }
+
+    function start () {
+        document.querySelector("#body").classList.add("move-bg");
+        document.querySelector(".fire").classList.remove("hidden");
+        allAudio.forEach(clip => {clip.play()})
+    }
+
+    function stop () {
+        document.querySelector("#body").classList.remove("move-bg");
+        document.querySelector(".fire").classList.add("hidden");
+        allAudio.forEach(clip => {clip.pause()})
+    }
 
     function allowDrag(event) {
         console.log('started draggin me');
@@ -22,12 +45,21 @@
         let droppedElId = event.dataTransfer.getData('draggedEl');
         let current = document.querySelector(`#${droppedElId}`);
         current.classList.toggle("hidden");
+        
+
+
+        let currentAudio = document.querySelector(`#${droppedElId}-audio`);
+        
+        //currentAudio.defaultMuted = false;
+
+        if (currentAudio.volume == 0) {currentAudio.volume = 1;
+        } else {currentAudio.volume = 0;
+        }
+
+        start()
 	}
 
-    function startStop () {
-        document.querySelector("#body").classList.toggle("move-bg");
-        document.querySelector(".fire").classList.toggle("hidden");
-    }
+
     
 
 
@@ -37,13 +69,11 @@
     jet.addEventListener('dragover', allowDragOver);
     jet.addEventListener('drop', allowDrop);
 
-    playBut.addEventListener('click',startStop);
-    pauseBut.addEventListener('click',startStop);
+    playBut.addEventListener('click',start);
+    pauseBut.addEventListener('click',stop);
 
 
-    jetItem.forEach(item => item.addEventListener('click',hide));
-
-
+    //jetItem.forEach(item => item.addEventListener('click',hide));
 
 
 
